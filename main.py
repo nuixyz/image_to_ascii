5709ea21bc97e16ba060beebdc9c5abf0b11a905
nuix09 <136358529+nuix09@users.noreply.github.com>
2024-10-20 01:23:11 +0530

    123

---
+++ main.py
@@ -0,0 +1,38 @@
+import PIL.Image as pil
+
+ASCII_CH = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+def resize_image(image, new_width = 100):
+  width, height = image.size
+  ratio = height/width
+  new_height = int(new_width * ratio) #this is to maintain the aspect ratio of the image
+  resized_image = image.resize((new_width, new_height))
+  return(resized_image)
+
+def grayify(image):
+  grayscale_image = image.convert("L")
+  return(grayscale_image)
+
+def pixels_to_ascii(image):
+  pixels = image.getdata()
+  characters = "".join([ASCII_CH[pixel//25] for pixel in pixels])
+  return(characters)
+
+def main(new_width = 100):
+  path = input("Enter the image path: ")
+  try:
+    image = pil.open(path)
+  except:
+    print(path,"is not a valid path name.")
+
+  new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+
+  pixel_count = len(new_image_data)
+  ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+  print(ascii_image)
+
+  with open("ascii_image.txt", "w") as f:
+    f.write(ascii_image)
+
+main()
